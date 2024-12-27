74c9366f9767eebf99837ae1d1be72463e5abc1b
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-27 14:58:50 +0530

    Update App.jsx

--- Client/Vaanoli/src/App.jsx
+++ Client/Vaanoli/src/App.jsx
@@ -1,28 +1,32 @@
-import axios from 'axios'
-import Player from './components/Player'
-import React, { useEffect,useState } from 'react'
+import axios from 'axios';
+import Player from './components/Player';
+import React, { useEffect, useState } from 'react';
+
 function App() {
-  const [Radio, setRadio] = useState([]);
-  const getData=async()=>{
-    var liveRadioListUrl = "https://gist.githubusercontent.com/valarpirai/473305f09f8433f1d338634ed42c437d/raw/live-radio.json?id=" + new Date().getTime();  
-    const data=await axios.get(liveRadioListUrl);
-    const combinedData = [...data.data[0].channels, ...data.data[1].channels];
+  const [radio, setRadio] = useState([]);
+  const getData = async () => {
+    const liveRadioListUrl =
+      "https://gist.githubusercontent.com/valarpirai/473305f09f8433f1d338634ed42c437d/raw/live-radio.json?id=" +
+      new Date().getTime();
+    const response = await axios.get(liveRadioListUrl);
+    const combinedData = [...response.data[0].channels, ...response.data[1].channels];
     setRadio(combinedData);
+  };
+  console.log(radio);
 
-}
-console.log(Radio);
-useEffect(()=>{
-   getData()
-},[]);
+  useEffect(() => {
+    getData();
+  }, []);
+  const selectedIndices = [1, 13, 17, 18, 21, 26, 27, 29, 30, 32, 33, 34, 36, 42, 44];
+  let filteredRadio = radio.filter((_, index) => selectedIndices.includes(index));
+  filteredRadio=[...filteredRadio].reverse();
   return (
-    <>
     <div>
-    {Radio.map((item, index) => (
-  <Player key={index} src={item.src} title={item.title} />
-    ))}
+      {filteredRadio.map((item, index) => (
+        <Player key={index} src={item.src} title={item.title} />
+      ))}
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
